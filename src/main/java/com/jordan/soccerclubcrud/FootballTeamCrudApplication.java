f51eecdaf99312ee20c0ad59e041296cbd3bc6fe
JordanBjr23 <jordanmarz12@gmail.com>
2023-06-07 09:58:10 -0600

    Se agrega el proyecto

---
+++ src/main/java/com/jordan/soccerclubcrud/FootballTeamCrudApplication.java
@@ -0,0 +1,13 @@
+package com.jordan.soccerclubcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTeamCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTeamCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jordan/soccerclubcrud/FootballTeamCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordan.soccerclubcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTeamCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
