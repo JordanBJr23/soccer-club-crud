47091e7e0bbb048f8bb3e82f7128b5e3b6d65ea2
JordanBjr23 <jordanmarz12@gmail.com>
2023-06-08 23:26:28 -0600

    CONTROLADORES

--- src/main/java/com/jordan/soccerclubcrud/modelo/Team.java
+++ src/main/java/com/jordan/soccerclubcrud/modelo/Team.java
@@ -1,2 +1,14 @@
-package com.jordan.soccerclubcrud.modelo;public class Team {
+package com.jordan.soccerclubcrud.modelo;
+
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+public class Team {
+    @NonNull
+    private String nombrePais;
+    @NonNull
+    private String confederacion;
+
+    private String rankingFifa;
 }
